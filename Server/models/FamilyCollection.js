dc8f183acaecfb3176d69f7dbffd701cbfcb0406
Your Name <you@example.com>
2018-02-07 11:31:15 -0800

    changed

--- Server/models/FamilyCollection.js
+++ Server/models/FamilyCollection.js
@@ -6,9 +6,11 @@ var Schema = mongoose.Schema;
 var FamilySchema=new Schema
 ({
     familyName:{type :String},
+	villageName:{type:String},
     memberName:[{
         name:{type: String},
         age:{type: Number},
+		gender:{type:String},
         occupation:{type: String}
     }],
     villageName:{type:String}
